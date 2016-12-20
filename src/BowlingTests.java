0625c3977cce6eb8c566aed9d0a7b6841e634e08
jacobOrtega <jenku18@gmail.com>
2016-12-20 23:17:14 +0200

    created insertScoreToFrame test

--- src/BowlingTests.java
+++ src/BowlingTests.java
@@ -1,4 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 
 public class BowlingTests {
-
+	
+	Frame frame = new Frame();
+	
+	@Test
+	public void setScoreToFrame(){
+		frame.setFramescore(1, 6);
+		assertThat(frame.getFramescore(), is(7));
+	}
 }
--- src/Frame.java
+++ src/Frame.java
@@ -1,4 +1,14 @@
 
 public class Frame {
 
+	public Object getFramescore() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setFramescore(int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
